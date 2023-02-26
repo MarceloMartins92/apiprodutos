aed2b4b0331176dc7821f5881a525d5f3410f68e
MarceloMartins92 <eu.marcelomartins@gmail.com>
2023-02-26 14:26:39 -0300

    first commit

---
+++ ApiProdutos.Services/Models/CategoriasGetModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiProdutos.Services.Models
+{
+    public class CategoriasGetModel
+    {
+        public Guid? IdCategoria { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+    }
+}
